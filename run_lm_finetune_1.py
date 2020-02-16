10b6d58dc7c686b879f9fa40721a6fa9400d25c5
Dmitry Zmitrovich <zmitrovich@gmail.com>
2020-02-16 15:37:49 +0300

    max_files_load

--- run_lm_finetune_1.py
+++ run_lm_finetune_1.py
@@ -613,7 +613,7 @@ def main():
     config_class, model_class, tokenizer_class = MODEL_CLASSES[args.model_type]
     config = config_class.from_pretrained(args.config_name if args.config_name else args.model_name_or_path)
     print("//////////////       //////////    ////////")
-    if args.tokenizer_class: tokenizer_class = globals()[args.tokenizer_class]
+
     tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,
                                                 do_lower_case=args.do_lower_case)
     if args.block_size <= 0:
